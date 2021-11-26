b2b29b768570b5a28d33934801b0e42e4bc1ebbe
Mennggen <menhu@ucm.es>
2021-11-26 01:57:36 +0100

    Poner los objetos clicables de room 1 2 3 y 4

--- src/room1.js
+++ src/room1.js
@@ -11,19 +11,21 @@ export default class Room1 extends BaseRoom{
     let dialogJson = this.cache.json.get('dialog');
     super.create();
 
-    this.ball=new clickableobjects(this,245,250,'objects','object_ball',true,dialogJson,"room1",0);
+    this.ball=new clickableobjects(this,200,220,'objects','object_ball',true,dialogJson,"room1",0);
  
-    this.cartoy =new clickableobjects(this,300,330,'objects', 'object_cartoy' ,true,dialogJson,"room1",1);
+    this.cartoy =new clickableobjects(this,150,250,'objects', 'object_cartoy' ,true,dialogJson,"room1",1);
 
-    this.books=new clickableobjects(this,315,110,'objects','object_books',true,dialogJson,"room1",2);
+    this.books=new clickableobjects(this,270,78,'objects','object_books',false,dialogJson,"room1",-1);
 
-    this.picture=new clickableobjects(this,140,85,'objects','object_picture',true,dialogJson,"room1",3);
+    this.picture=new clickableobjects(this,50,60,'objects','object_picture',false,dialogJson,"room1",-1);
 
-    this.drawing=new clickableobjects(this,900,200,'objects','object_drawing',true,dialogJson,"room1",4);
+    this.drawing=new clickableobjects(this,800,130,'objects','object_drawing',false,dialogJson,"room1",-1);
 
-    this.bag=new clickableobjects(this,780,350,'objects','object_bag',true,dialogJson,"room1",5);
+    this.bag=new clickableobjects(this,700,250,'objects','object_bag',false,dialogJson,"room1",-1);
 
-    this.objectarray = [this.ball,this.cartoy,this.books,this.picture,this.drawing,this.bag];
+    this.sara=new clickableobjects(this,100,130,'objects','object_sara',false,dialogJson,"room1",-1);
+    this.smith=new clickableobjects(this,750,100,'objects','object_smith',false,dialogJson,"room1",-1);
+    this.objectarray = [this.ball,this.cartoy,this.books,this.picture,this.drawing,this.bag,this.sara,this.smith];
 
     this.objectarray.forEach(elem => elem.setInteractive()
     .setOrigin(0,0)
