e864601d3dfa3b4ea768c84a53e2dcafbf39993e
javimuno <javimuno@ucm.es>
2021-12-12 17:00:35 +0100

    json-s- de los días 1,2 y 3 completos, y deducciones y conclusiones de los mismos días
    Se han creado ya todos los eventos, deducciones y conclusiones en formato json de los días 1,2 y 3 a falta de añadir los id en los eventos importantes

--- src/pregunta.js
+++ src/pregunta.js
@@ -61,7 +61,7 @@ export default class Pregunta extends Phaser.Scene{
         muestrapregunta(){
 
           this.pregunta_array=["¿Qué ha pasado en realidad con Kevin el hijo de la familia Smith?","¿Dónde está Candy, el perro de la señora Cooper?",
-          "¿Qué relación tiene el detective Charles Doyle con el resto de vecinos?","¿Cómo es la relación del Profesor William Scott con el resto de vecinos?",
+          "¿Qué relación intrínseca existe entre el matrimonio Smith, el detective Charles Doyle y el profesor William Scott?","¿Cómo es la relación del Profesor William Scott con el resto de vecinos?",
           "¿Quién ha matado al profesor William Scott?"]
 
           //muestra el texto de la pregunta
