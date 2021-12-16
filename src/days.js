771a139c7ca4bcd83b59d4d0cfd61bb541c5aec4
javimuno <javimuno@ucm.es>
2021-12-16 12:35:58 +0100

    eliminado comentario largo de days, ya solo queda bucle gestor

--- src/days.js
+++ src/days.js
@@ -24,40 +24,6 @@ export default class Days extends Phaser.Scene{
             }
         }
         return objectreturned ;
-
     }
-
-//     day1(scene,room){       
-//         else if(room==="room2"){
-//             let tetera =new clickableobjects(scene,340,125,'objects', 'object_cup',dialogJson,"room2",0);
-
-//             let cat = new clickableobjects(scene,200,310,'objects', 'object_cat',dialogJson,"room2",1);
-           
-//             let dog=new clickableobjects(scene,300,270,'objects', 'object_dog',dialogJson,"room2",2);
-            
-//             let aguja=new clickableobjects(scene,60,260,'objects', 'object_agujas',dialogJson,"room2",3);
-     
-//             let tejido=new clickableobjects(scene,80,200,'objects', 'object_tejido',dialogJson,"room2",4);
-     
-//             let ovillo=new clickableobjects(scene,90,320,'objects', 'object_ovillo',dialogJson,"room2",5);
-     
-//             let cooper=new clickableobjects(scene,540,120,'objects', 'object_cooper',dialogJson,"room2",6);
-     
-//             let objectarray = [tetera,cat,dog,aguja,tejido,ovillo,cooper];
-//             return objectarray;
-//         }
-//         else if(room==="room3"){
-            
-//             let charles =new clickableobjects(scene,250,200,'objects', 'object_charles',dialogJson,"room3",0);
-//             let objectarray = [charles];
-//             return objectarray;
-//         }
-//         else if(room==="room4"){
-//             let william =new clickableobjects(scene,600,130,'objects', 'object_william',dialogJson,"room4",0);
-
-//             let objectarray = [william];
-//             return objectarray;
-//         }
-//     }
 }
 
