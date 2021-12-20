b38529b7abe5b26bdaa49fe4d767aa432167052e
javimuno <javimuno@ucm.es>
2021-12-20 02:53:59 +0100

    comentado menú y cambiado el tamaño y la posición del mensaje de bienvenida

--- src/menu.js
+++ src/menu.js
@@ -1,12 +1,16 @@
+/**
+ * Clase que crea la primera escena del juego cuando este arranca, con un mensaje de bienvenida
+ * y un único botón que permite cambiar a la escena general del juego
+ */
 export default class menu extends Phaser.Scene{
     constructor() {
         super({ key: 'menu' });
       }  
-      //Crea el texto de bienvenida
+      //Crea el texto de bienvenida y botón de play
     create(){
      this.background = this.add.image(0,0,'houses');
      this.background.setOrigin(0,0);
-     this.add.bitmapText(200, 400, 'press_start_2p_font', "Bienvenidos a Through The Window!", 20).setOrigin(0,1);
+     this.add.bitmapText(10, 490, 'press_start_2p_font', "Bienvenidos a Through The Window!", 30).setOrigin(0,1);
   
      let startButton = this.add.sprite(500,250,'play')
      this.input.mouse.disableContextMenu();
